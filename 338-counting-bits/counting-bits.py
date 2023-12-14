afa52159196caa7595fd00c816078be873744f6f
err0-0rr <126408020+err0-0rr@users.noreply.github.com>
2023-12-14 19:42:37 +0530

    Time: 92 ms (33.42%) | Memory: 23.1 MB (17.78%) - LeetSync

---
+++ 338-counting-bits/counting-bits.py
@@ -0,0 +1,16 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        ans=[0]*(n+1)
+        def util(x):
+            t=0
+            while x:
+                t+=x%2
+                x>>=1
+                if ans[x]!=0:
+                    t+=ans[x]
+                    break
+            return t
+            
+        for i in range(n+1):
+            ans[i]=util(i)
+        return ans
