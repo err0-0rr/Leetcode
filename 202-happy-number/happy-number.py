774c2ef4566530718c274d393276da1439e2f458
err0-0rr <126408020+err0-0rr@users.noreply.github.com>
2023-12-14 19:32:13 +0530

    Time: 51 ms (11.12%) | Memory: 16.4 MB (18.73%) - LeetSync

---
+++ 202-happy-number/happy-number.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        def util(n):
+            t=0
+            while n:
+                t+=(n%10)**2
+                n//=10
+            return t
+            
+        s=set()
+        while n!=1:
+            if n not in s:
+                s.add(n)
+            else:
+                return False
+            n=util(n)
+        return True
