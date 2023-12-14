2f3db1395e3e69de6cc8bc83f12bcbc412e6a8ca
err0-0rr <126408020+err0-0rr@users.noreply.github.com>
2023-12-14 20:36:21 +0530

    Time: 45 ms (60.22%) | Memory: 16.4 MB (20.95%) - LeetSync

---
+++ 768-partition-labels/partition-labels.py
@@ -0,0 +1,18 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        end={}
+        for i in range(len(s)-1, -1, -1):
+            if s[i] not in end:
+                end[s[i]]=i
+        
+        ans=[]      
+        t=set()
+        tot=-1
+        for i in range(len(s)):
+            t.add(s[i])
+            if i==end[s[i]]:
+                t.remove(s[i])
+            if not t:
+                ans.append(i-tot)
+                tot=i
+        return ans
