64ff3da97c632b92016576e4426eea1054df81eb
err0-0rr <126408020+err0-0rr@users.noreply.github.com>
2023-12-14 19:21:44 +0530

    Time: 54 ms (9.80%) | Memory: 18.6 MB (68.36%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
@@ -7,7 +7,7 @@
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         if not root:
-                return 0
+            return 0
         l=self.maxDepth(root.left)
         r=self.maxDepth(root.right)
         return 1+max(l,r)
