df7acbe015ed65c1fb890afa16674923be02affc
err0-0rr <126408020+err0-0rr@users.noreply.github.com>
2023-12-14 19:25:27 +0530

    Time: 48 ms (33.44%) | Memory: 16.9 MB (14.68%) - LeetSync

---
+++ 133-clone-graph/clone-graph.py
@@ -0,0 +1,38 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+from typing import Optional
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if not node:
+            return None
+        m={}
+        done=set()
+        visited=set() 
+        q=deque()
+        q.append(node)
+        while q:
+            a=q.popleft()
+            if not a:
+                continue
+            if a.val not in m:
+                m[a.val]=Node(a.val)
+            
+            if a.val not in done:
+                for i in a.neighbors:
+                    if i.val not in m:
+                       m[i.val] =Node(i.val)
+                    m[a.val].neighbors.append(m[i.val])
+                done.add(a.val)
+                
+            for i in a.neighbors:
+                if i not in visited:
+                    visited.add(i)
+                    q.append(i)
+                
+        return m[node.val]
