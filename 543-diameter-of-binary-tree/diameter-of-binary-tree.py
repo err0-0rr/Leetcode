061870358a71c0794a1aff847d597d42e034af21
err0-0rr <126408020+err0-0rr@users.noreply.github.com>
2023-12-14 19:49:50 +0530

    Time: 54 ms (25.03%) | Memory: 18.7 MB (39.78%) - LeetSync

---
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        ans=0
+        def util(head):
+            if not head:
+                return 0
+            a=util(head.left)
+            b=util(head.right)
+            nonlocal ans
+            ans=max(ans, a+b)
+            return max(a,b)+1
+        util(root)
+        return ans
+
+        
