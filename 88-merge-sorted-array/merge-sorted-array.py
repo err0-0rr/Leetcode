3e3b56a2fd7188c990ef150549a876dba7fe7dc3
err0-0rr <126408020+err0-0rr@users.noreply.github.com>
2023-12-14 19:15:26 +0530

    Time: 50 ms (17.36%) | Memory: 16.3 MB (72.41%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        i,j=m-1,n-1
+        idx=i+j+1
+        while idx>=0:
+            if j>=0 and (nums2[j]>=nums1[i] or i<0):
+                nums1[idx]=nums2[j]
+                j-=1
+            else:
+                nums1[idx]=nums1[i]
+                i-=1
+            idx-=1
+            
