12a67dced5c5bd1454d6db0df673f37429397fc0
err0-0rr <126408020+err0-0rr@users.noreply.github.com>
2023-12-14 19:12:39 +0530

    Time: 65 ms (12.82%) | Memory: 17.1 MB (6.99%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
@@ -1,5 +1,5 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> bool:
-        if target not in nums:
+        if target not in set(nums):
             return False
         return True
