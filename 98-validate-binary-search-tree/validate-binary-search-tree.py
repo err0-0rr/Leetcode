601564d95f179ae22f752c77a1deb1a114064662
err0-0rr <126408020+err0-0rr@users.noreply.github.com>
2023-12-14 19:17:59 +0530

    Time: 52 ms (32.13%) | Memory: 19.6 MB (7.94%) - LeetSync

---
+++ 98-validate-binary-search-tree/validate-binary-search-tree.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        s=float("-inf")
+        ans=True
+        def util(r):
+            nonlocal s
+            if r.left:
+                util(r.left)
+            if s>=r.val:
+                nonlocal ans
+                ans=False
+                return 
+            else:
+                s=r.val
+            if r.right:
+                util(r.right)
+        util(root)
+        return ans
