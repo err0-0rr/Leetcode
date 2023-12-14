20126ffd6c46e1193d27c5dc94c7287df0864b22
err0-0rr <126408020+err0-0rr@users.noreply.github.com>
2023-12-14 19:27:56 +0530

    Time: 79 ms (60.85%) | Memory: 16.8 MB (43.43%) - LeetSync

---
+++ 152-maximum-product-subarray/maximum-product-subarray.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        ans=max(nums)
+        mini=maxi=1
+        for i in nums:
+            if i==0:
+                mini=maxi=1
+                continue
+            ls=[mini*i, maxi*i, i]
+            mini,maxi=min(ls), max(ls)
+            ans=max(ans, maxi)
+        return ans
