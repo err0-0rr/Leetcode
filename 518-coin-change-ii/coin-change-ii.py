5282e2961d78e5109e38247ca242f9debceab678
err0-0rr <126408020+err0-0rr@users.noreply.github.com>
2023-12-14 19:49:23 +0530

    Time: 437 ms (33.53%) | Memory: 32.2 MB (39.10%) - LeetSync

---
+++ 518-coin-change-ii/coin-change-ii.py
@@ -0,0 +1,13 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        dp=[[0 for i in range(amount+1)] for j in range(len(coins)+1)]
+        for i in range(len(dp)):
+            dp[i][0]=1
+            
+        for i in range(1,len(dp)):
+            for j in range(1,len(dp[0])):
+                dp[i][j]=dp[i-1][j]
+                if j-coins[i-1]>=0:
+                    dp[i][j]+=dp[i][j-coins[i-1]]
+                    
+        return dp[-1][-1]
