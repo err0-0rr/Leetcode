e38ab1d7d2a06987716323dc8a9ca413ae1b0461
err0-0rr <126408020+err0-0rr@users.noreply.github.com>
2023-12-14 19:55:35 +0530

    Time: 77 ms (98.18%) | Memory: 16.2 MB (65.89%) - LeetSync

---
+++ 647-palindromic-substrings/palindromic-substrings.py
@@ -0,0 +1,16 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        ans=0
+        n=len(s)
+        for i in range(n):
+            l=r=i
+            while r<n and s[l]==s[r]:
+                ans+=1
+                r+=1
+            l-=1
+            while l>=0 and r<n and s[l]==s[r]:
+                ans+=1
+                l-=1
+                r+=1
+                
+        return ans
