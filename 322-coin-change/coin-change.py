fc8651686f4841ebd9c1768c51ef94ba2ec022a1
err0-0rr <126408020+err0-0rr@users.noreply.github.com>
2023-12-14 19:42:11 +0530

    Time: 1083 ms (34.98%) | Memory: 16.8 MB (47.05%) - LeetSync

---
+++ 322-coin-change/coin-change.py
@@ -0,0 +1,11 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp=[float("inf")]*(amount+1)
+        dp[0]=0
+        for idx, val in enumerate(dp):
+            for i in coins:
+                if idx-i>=0:
+                    dp[idx]=min(dp[idx], dp[idx-i]+1)
+        if dp[amount]==float("inf"):
+            return -1
+        return dp[amount]
