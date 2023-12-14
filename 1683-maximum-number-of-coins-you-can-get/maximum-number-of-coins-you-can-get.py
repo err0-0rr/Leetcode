60fbb434ff22c96d439c708c9901136ba2665b0f
err0-0rr <126408020+err0-0rr@users.noreply.github.com>
2023-12-14 20:48:30 +0530

    Time: 534 ms (31.42%) | Memory: 29 MB (21.56%) - LeetSync

---
+++ 1683-maximum-number-of-coins-you-can-get/maximum-number-of-coins-you-can-get.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxCoins(self, piles: List[int]) -> int:
+        piles.sort()
+        n=len(piles)-2
+        c=n//3
+        ans=0
+        while c>=0:
+            ans+=piles[n]
+            n-=2
+            c-=1
+        return ans
