b27dcce4e9c07cc159236aefd9a479ce0d3b949a
err0-0rr <126408020+err0-0rr@users.noreply.github.com>
2024-03-05 19:36:28 +0530

    Time: 168 ms (34.59%) | Memory: 28 MB (54.19%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -3,9 +3,13 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        k=k%len(nums)
-        
-        while k>0:
-            temp=nums.pop()
-            nums.insert(0, temp)
-            k-=1
+        n=len(nums)
+        k=(k%n)
+        def reverse(l, r):
+            while l<r:
+                nums[l],nums[r]=nums[r],nums[l]
+                l+=1
+                r-=1
+        reverse(0, n-1)
+        reverse(0, k-1)
+        reverse(k, n-1)
