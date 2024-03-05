bbc1f61578cdfa6efc8da0444211fd89a88760dd
err0-0rr <126408020+err0-0rr@users.noreply.github.com>
2024-03-05 19:08:04 +0530

    Time: 1123 ms (14.86%) | Memory: 27.9 MB (78.76%) - LeetSync

---
+++ 189-rotate-array/rotate-array.py
@@ -0,0 +1,11 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k=k%len(nums)
+        
+        while k>0:
+            temp=nums.pop()
+            nums.insert(0, temp)
+            k-=1
