a46263423c015905cc7cf2a21fd464ed220a8862
err0-0rr <126408020+err0-0rr@users.noreply.github.com>
2024-03-27 21:31:54 +0530

    Time: 586 ms (36.83%) | Memory: 32.2 MB (87.53%) - LeetSync

---
+++ 3151-minimum-processing-time/minimum-processing-time.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minProcessingTime(self, processorTime: List[int], tasks: List[int]) -> int:
+        processorTime.sort()
+        tasks.sort(reverse=True)
+        
+        ans=0
+        temp=0
+        for i in processorTime:
+            for j in range(4):
+                ans=max(ans, i+tasks[temp+j])
+            temp+=4
+        return ans
